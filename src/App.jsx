cf5499bf554cc0c5ef2f1dbca8e5d1dbb781f4d8
MostafaSafwatIS21 <mostafa.safwat.is.fcai@gmail.com>
2026-05-04 13:53:21 +0300

    start react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="text-red-500">Helo React</div>;
+}
+
+export default App;
